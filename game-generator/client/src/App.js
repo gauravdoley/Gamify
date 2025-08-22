2db0fc7a8e0515b21521acfd3546b63a40559440
Gaurav Doley <doley6gaurav@gmail.com>
2025-08-22 11:54:34 +0530

    Convert client submodule to a regular directory

---
+++ game-generator/client/src/App.js
@@ -0,0 +1,13 @@
+// client/src/App.js
+import React from 'react';
+import './index.css'; // Make sure this line exists to import your CSS
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Hello Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
